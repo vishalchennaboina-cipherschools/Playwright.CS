7a08ddf211d0b637981694fd51e00a9b32df4c9d
vishalchennaboina <vishal.chennaboina@cipherschools.com>
2026-07-14 23:04:41 +0530

    feat: implement spec discovery route and add logging for listSpecs endpoint

--- backend/src/routes/spec.routes.js
+++ backend/src/routes/spec.routes.js
@@ -6,11 +6,25 @@
  * @module routes/spec.routes
  */
 
+// const { Router } = require('express');
+// const ctrl = require('../controllers/spec.controller');
+
+// const router = Router();
+
+// router.get('/specs', ctrl.listSpecs);
+
+// module.exports = router;
+
 const { Router } = require('express');
 const ctrl = require('../controllers/spec.controller');
 
 const router = Router();
 
-router.get('/specs', ctrl.listSpecs);
+console.log('[Routes] spec.routes loaded');
+
+router.get('/specs', (req, res, next) => {
+    console.log('[Routes] GET /api/specs');
+    next();
+}, ctrl.listSpecs);
 
 module.exports = router;
